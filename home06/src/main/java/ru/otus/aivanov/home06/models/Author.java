50c74516c80e9467d3a328d34788f778487ad967
Anton Ivanov <rellai@yandex.ru>
2023-11-21 00:26:18 +0300

    шестая ДЗ

--- home06/src/main/java/ru/otus/aivanov/home06/models/Author.java
+++ home06/src/main/java/ru/otus/aivanov/home06/models/Author.java
@@ -1,10 +1,6 @@
 package ru.otus.aivanov.home06.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 
@@ -12,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity(name = "authors")
+@Table(name = "authors")
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
