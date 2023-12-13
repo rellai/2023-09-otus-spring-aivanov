1f7cd4c64648470ba89d52254e1bcd9cf5f55056
Anton Ivanov <rellai@yandex.ru>
2023-12-13 10:18:40 +0300

    Восьмая ДЗ

--- home08/src/main/java/ru/otus/aivanov/home08/repositories/AuthorRepository.java
+++ home08/src/main/java/ru/otus/aivanov/home08/repositories/AuthorRepository.java
@@ -1,6 +1,5 @@
 package ru.otus.aivanov.home08.repositories;
 
-import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.otus.aivanov.home08.models.Author;
