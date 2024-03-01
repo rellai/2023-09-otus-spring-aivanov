59b3247878c02101b31744cbba8fd0811e3b132d
Anton Ivanov <rellai@yandex.ru>
2024-03-02 01:18:34 +0300

    восемнадцатая дз

---
+++ home18/client-service/src/main/java/ru/otus/aivanov/home18/client/config/LoadBalancerConfig.java
@@ -0,0 +1,15 @@
+package ru.otus.aivanov.home18.client.config;
+
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class LoadBalancerConfig {
+    @Bean
+    public ServiceInstanceListSupplier instanceSupplier(ConfigurableApplicationContext context) {
+        return ServiceInstanceListSupplier.builder()
+                .withDiscoveryClient()
+                .withHealthChecks()
+                .build(context);
+    }
+}
